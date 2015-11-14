86bed4113ba036bb1e8d0f81c2c40d09c13abb4c
AlexandrePetermann <xandy.bq@gmail.com>
2015-11-13 23:41:53 -0200

    closes #2

---
+++ Index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+/* 
+    Created on : 13/11/2015, 23:35:02
+    Author     : Alexandre
+*/
+
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>Site Institucional do SENAI - Brusque</title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
---
+++ css/estiloso.css
@@ -0,0 +1,5 @@
+/* 
+    Created on : 13/11/2015, 23:35:02
+    Author     : Alexandre
+*/
+
