819e9a29ce3c3cfca7a211e07288529e62353380
Alexandre <xandy.bq@gmail.com>
2015-11-17 21:54:24 -0200

    Criado Grid aonde foi adicionado as informações para preencher o contato closes #29

--- Index.php
+++ Index.php
@@ -42,13 +42,13 @@
                 <div>
                     <div class="collapse navbar-collapse" id="myNavbar">
                         <ul class="nav navbar-nav">
-                            <li><a href="#section1">Inicio</a></li>
-                            <li><a href="#section2">Cursos</a></li>
-                            <li><a href="#section3">Notícias</a></li>
-                            <li><a href="#section4">Fotos</a></li>
-                            <li><a href="#section5">Videos</a></li>
-                            <li><a href="#section6">Contato</a></li>
-                            <li><a href="#section7">Localização</a></li>
+                            <li><a href="#Inicio">Inicio</a></li>
+                            <li><a href="#Cursos">Cursos</a></li>
+                            <li><a href="#Noticias">Notícias</a></li>
+                            <li><a href="#Fotos">Fotos</a></li>
+                            <li><a href="#Videos">Videos</a></li>
+                            <li><a href="#Contato">Contato</a></li>
+                            <li><a href="#Localizacao">Localização</a></li>
                             <!-- Menu DorpDown
                              <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Section 4 <span class="caret"></span></a>
                                  <ul class="dropdown-menu">
@@ -63,7 +63,7 @@
             </div>
         </nav>
 
-        <div id="section1" class="container-fluid">
+        <div id="Inicio" class="container-fluid">
             <h1>Inicio</h1>
             <!--Carrosel de Imagens-->
             <div id="myCarousel" class="carousel slide" data-ride="carousel">
@@ -105,31 +105,64 @@
                 </a>
             </div>
         </div>
-        <div id="section2" class="container-fluid">
+        <div id="Cursos" class="container-fluid">
             <h1>Cursos</h1>
             <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
             <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
         </div>
-        <div id="section3" class="container-fluid">
+        <div id="Noticias" class="container-fluid">
             <h1>Notícias</h1>
             <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
             <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
         </div>
-        <div id="section4" class="container-fluid">
+        <div id="Fotos" class="container-fluid">
             <h1>Fotos</h1>
             <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
             <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
         </div>
-        <div id="section5" class="container-fluid">
+        <div id="Videos" class="container-fluid">
             <h1>Videos</h1>
             <iframe width="90%" height="80%" src="https://www.youtube.com/embed/videoseries?list=PLVpmN2T0ZVYXObtTizne3LU1hKKBgE5_U" frameborder="0" allowfullscreen></iframe>
         </div>
-        <div id="section6" class="container-fluid">
+        <div id="Contato" class="container-fluid">
             <h1>Contato</h1>
-            <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
-            <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+
+            <div class="row">
+                <div class="col-lg-4">
+                    <p>Entre em contato, responderemos nas próximas 24 horas.</p>
+                    <p><span class="glyphicon glyphicon-map-marker"><b> Brusque/SC</b></span></p>
+                    <p><span class="glyphicon glyphicon-phone-alt"><b> (47)3251-8900</b></span></p>
+                    <p><span class="glyphicon glyphicon-envelope"><b> brusque@fisc.com.br</b></span></p>
+                    
+                </div>
+                <div class="col-lg-8">
+                    <form role="form">
+                        <div class="form-inline">                    
+                            <div class= "input-group" > 
+                                <span class="input-group-addon">
+                                    <span class="glyphicon glyphicon-user"></span>
+                                </span>
+                                <input  type= "text"  class= "form-control"  placeholder= "Nome"  aria-describedby= "basic-addon1" >                        
+                            </div>
+                            <div  class= "input-group" > 
+                                <span class="input-group-addon">
+                                    <span class="glyphicon glyphicon-envelope"></span>
+                                </span>
+                                <input  type= "email"  class= "form-control"  placeholder= "E-mail"  aria-describedby= "basic-addon1" >                        
+                            </div>
+                        </div>
+                        <div  class= "input-group"> 
+                            <span class="input-group-addon">
+                                <span class="glyphicon glyphicon-pencil"></span>
+                            </span>
+                            <textarea rows="13" class= "form-control"  placeholder= "Comentário"  aria-describedby= "basic-addon1" ></textarea>
+                        </div>
+                        <input type="submit" class="btn btn-default" value="Enviar">                        
+                    </form>
+                </div>
+            </div>
         </div>
-        <div id="section7" class="container-fluid">
+        <div id="Localizacao" class="container-fluid">
             <h1>Localização </h1>
             <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d28411.02199277725!2d-48.9140747472448!3d-27.112909543324925!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0000000000000000%3A0x0b0ebeaaf853b447!2sSENAI+-+Servi%C3%A7o+Nacional+de+Aprendizagem+Industrial!5e0!3m2!1spt-BR!2sbr!4v1447717805526" width="90%" height="80%" frameborder="0" style="border:0" allowfullscreen></iframe>
         </div>
