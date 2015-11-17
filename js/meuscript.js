62db567e30be78262a7f2a2831a478042857f70f
Aluno <Aluno@10.1.20.89>
2015-11-16 22:56:39 -0200

    Adicionado o Carrosel, #17
    http://www.w3schools.com/bootstrap/bootstrap_carousel.asp

--- js/meuscript.js
+++ js/meuscript.js
@@ -1,27 +1,26 @@
 /* 
-    Created on : 13/11/2015, 23:35:02
-    Author     : Alexandre
-*/
+ Created on : 13/11/2015, 23:35:02
+ Author     : Alexandre
+ */
 
 // Add scrollspy to <body>
-$('body').scrollspy({target: ".navbar", offset: 50}); 
+$('body').scrollspy({target: ".navbar", offset: 50});
 
 // Add smooth scrolling to all links inside a navbar
-$("#myNavbar a").on('click', function(event){
+$("#myNavbar a").on('click', function (event) {
 
-  // Prevent default anchor click behavior
-  event.preventDefault();
+    // Prevent default anchor click behavior
+    event.preventDefault();
 
-  // Store hash (#)
-  var hash = this.hash;
+    // Store hash (#)
+    var hash = this.hash;
+    // Using jQuery's animate() method to add smooth page scroll
+    // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area (the speed of the animation)
+    $('html, body').animate({
+        scrollTop: $(hash).offset().top
+    }, 800, function () {
 
-  // Using jQuery's animate() method to add smooth page scroll
-  // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area (the speed of the animation)
-  $('html, body').animate({
-    scrollTop: $(hash).offset().top
-  }, 800, function(){
-
-    // Add hash (#) to URL when done scrolling (default click behavior)
-    window.location.hash = hash;
-  });
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+    });
 });
