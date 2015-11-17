4a5f9178cad11692350aa06b2e934bf8d62416e1
Alexandre Petermann <xandy.bq@gmail.com>
2015-11-16 22:22:30 -0200

    {Smooth scrolling}(http://www.w3schools.com/bootstrap/bootstrap_ref_js_scrollspy.asp) closes #7

--- js/meuscript.js
+++ js/meuscript.js
@@ -3,3 +3,25 @@
     Author     : Alexandre
 */
 
+// Add scrollspy to <body>
+$('body').scrollspy({target: ".navbar", offset: 50}); 
+
+// Add smooth scrolling to all links inside a navbar
+$("#myNavbar a").on('click', function(event){
+
+  // Prevent default anchor click behavior
+  event.preventDefault();
+
+  // Store hash (#)
+  var hash = this.hash;
+
+  // Using jQuery's animate() method to add smooth page scroll
+  // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area (the speed of the animation)
+  $('html, body').animate({
+    scrollTop: $(hash).offset().top
+  }, 800, function(){
+
+    // Add hash (#) to URL when done scrolling (default click behavior)
+    window.location.hash = hash;
+  });
+});
