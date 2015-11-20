a574c8115e16c800b7661b27db061f080367e11b
Alexandre Petermann <xandy.bq@gmail.com>
2015-11-20 20:25:19 -0200

    Formulário de contato #30

--- Index.php
+++ Index.php
@@ -185,42 +185,7 @@
             <h1>Videos</h1>
             <iframe width="90%" height="80%" src="https://www.youtube.com/embed/videoseries?list=PLVpmN2T0ZVYXObtTizne3LU1hKKBgE5_U" frameborder="0" allowfullscreen></iframe>
         </div>
-        <div id="Contato" class="container-fluid">
-            <h1 class="text-center">Contato</h1>
-            <div class="row">
-                <div class="col-lg-4">
-                    <p>Entre em contato, responderemos nas próximas 24 horas.</p>
-                    <p><i class="glyphicon glyphicon-map-marker"><b> Brusque/SC</b></i></p>
-                    <p><i class="glyphicon glyphicon-phone-alt"><b> (47)3251-8900</b></i></p>
-                    <p><i class="glyphicon glyphicon-envelope"><b> brusque@fisc.com.br</b></i></p>
-                </div>
-                <div class="col-lg-8">
-                    <form role="form">
-                        <div class="form-inline">                    
-                            <div class= "input-group" > 
-                                <span class="input-group-addon">
-                                    <span class="glyphicon glyphicon-user"></span>
-                                </span>
-                                <input  type= "text"  class= "form-control"  placeholder= "Nome" >                        
-                            </div>
-                            <div  class= "input-group" > 
-                                <span class="input-group-addon">
-                                    <span class="glyphicon glyphicon-envelope"></span>
-                                </span>
-                                <input  type= "email"  class= "form-control"  placeholder= "E-mail">                        
-                            </div>
-                        </div>
-                        <div  class= "input-group"> 
-                            <span class="input-group-addon">
-                                <span class="glyphicon glyphicon-pencil"></span>
-                            </span>
-                            <textarea rows="13" class= "form-control"  placeholder= "Comentário"  aria-describedby= "basic-addon1" ></textarea>
-                        </div>
-                        <input type="submit" class="btn btn-default" value="Enviar">                        
-                    </form>
-                </div>
-            </div>
-        </div>
+        <?php include 'formContato.php';?>
         <div id="Localizacao" class="container-fluid text-center">
             <h1>Localização </h1>
             <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d28411.02199277725!2d-48.9140747472448!3d-27.112909543324925!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0000000000000000%3A0x0b0ebeaaf853b447!2sSENAI+-+Servi%C3%A7o+Nacional+de+Aprendizagem+Industrial!5e0!3m2!1spt-BR!2sbr!4v1447717805526" width="90%" height="80%" frameborder="0" style="border:0" allowfullscreen></iframe>
---
+++ formContato.php
@@ -0,0 +1,31 @@
+<section id="Contato" class="container-fluid">
+    <h1 class="text-center">Contato</h1>
+    <div class="row">
+        <div class="col-lg-4">
+            <p>Entre em contato, responderemos nas próximas 24 horas.</p>
+            <p><i class="glyphicon glyphicon-map-marker"><b> Brusque/SC</b></i></p>
+            <p><i class="glyphicon glyphicon-phone-alt"><b> (47)3251-8900</b></i></p>
+            <p><i class="glyphicon glyphicon-envelope"><b> brusque@fisc.com.br</b></i></p>
+        </div>
+        <div class="col-lg-8">
+            <form role="form" id="contactForm">
+                <div class="row">
+                    <div class="form-group col-sm-6">
+                        <label for="name" class="h4">Nome</label>
+                        <input type="text" class="form-control" id="name" placeholder="Informe seu nome" required>
+                    </div>
+                    <div class="form-group col-sm-6">
+                        <label for="email" class="h4">E-mail</label>
+                        <input type="email" class="form-control" id="email" placeholder="Informe seu e-mail" required>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="message" class="h4 ">Mensagem</label>
+                    <textarea id="message" class="form-control" rows="5" placeholder="Informe uma Mensagem" required></textarea>
+                </div>
+                <button type="submit" id="form-submit" class="btn btn-success btn-lg pull-right ">Enviar</button>
+                <div id="msgSubmit" class="h3 text-center hidden">Mensagem Enviada!</div>
+            </form>
+        </div>
+    </div>
+</section>
