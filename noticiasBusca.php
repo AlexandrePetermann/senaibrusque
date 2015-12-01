bb3e0976033287155b3d17b71e8d6cb38a56bd71
Alexandre <xandy.bq@gmail.com>
2015-12-01 13:45:52 -0200

    Montagem para apresentar diversas noticias dinamicamente #23

--- noticiasBusca.php
+++ noticiasBusca.php
@@ -10,11 +10,36 @@ try {
 
 //Busca noticia do banco de dados
 // Listagem de dados
-$sql = "select titulo from noticias order by datapostagem desc";
+$sql = "select titulo, resumo, noticia from noticias order by datapostagem desc";
 $q = $conn->query($sql);
 $q->setFetchMode(PDO::FETCH_ASSOC);
 
-
 while ($r = $q->fetch()) {
-    echo "<h1>".$r['titulo']."</h1>";
+    $String = " 
+    <div class='row'>
+        <div class='col-xs-8 col-md-4'>            
+            <h1>" . $r['titulo'] . "</h1>
+            <h2>" . $r['resumo'] . "</h2>
+            <button type='button' class='btn btn-info btn-lg' data-toggle='modal' data-target='#myModal'>Ler mais...</button>
+            <!-- Modal -->
+            <div class='modal fade' id='myModal' role='dialog'>
+                <div class='modal-dialog'>
+                    <div class='modal-content'>
+                        <div class='modal-header'>
+                            <button type='button' class='close' data-dismiss='modal'>&times;</button>
+                            <h4 class='modal-title'>" . $r['titulo'] . "</h4>
+                        </div>                        
+                        <div class='modal-body'>
+                           <h1>" . $r['noticia'] . "</h1>
+                        </div>
+                        <div class='modal-footer'>
+                            <button type='button' class='btn btn-default' data-dismiss='modal'>Sair</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>        
+    </div>
+";
+    echo $String;
 }
--- noticiasForm.php
+++ noticiasForm.php
@@ -2,35 +2,6 @@
 
 <section id="Noticias" class="container-fluid">
     <h1>Notícias</h1>
-    <div class="row">
-        <div class="col-xs-8 col-md-4">
-            Notícia 01
-        </div>
-        <div class="col-xs-8 col-md-4">
-            <?php include './noticiasBusca.php'; ?>            
-            <!-- Trigger the modal with a button -->
-            <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Ler mais...</button>
-            <!-- Modal -->
-            <div class="modal fade" id="myModal" role="dialog">
-                <div class="modal-dialog">
-                    <!-- Modal content-->
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <button type="button" class="close" data-dismiss="modal">&times;</button>
-                            <h4 class="modal-title">Título da noticia 02</h4>
-                        </div>                        
-                        <div class="modal-body">
-                           
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-default" data-dismiss="modal">Sair</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>        
-        <div class="col-xs-8 col-md-4">
-            Notícia 03
-        </div>
-    </div>
+    <?php include './noticiasBusca.php'; ?> 
+
 </section>
