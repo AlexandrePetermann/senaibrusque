6e6a3f76bb3f2689881c0fa05a8c69f15fb2e929
Alexandre <xandy.bq@gmail.com>
2015-12-02 22:17:42 -0200

    Alterado para retirar do index a informação das fotos

---
+++ fotosForm.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<section id="Fotos" class="container-fluid">
+            <h1>Fotos</h1>
+            <div class="row">
+                <div class="col-xs-6 col-md-3">
+                    <a href="#" class="thumbnail">
+                        <img src="http://placehold.it/350x120">
+                    </a>
+                    <a href="#" class="thumbnail">
+                        <img src="http://placehold.it/350x120">
+                    </a>
+                    <a href="#" class="thumbnail">
+                        <img src="http://placehold.it/350x120">
+                    </a>                    
+                </div>
+                <div class="col-xs-6 col-md-3">
+                    <a href="#" class="thumbnail">
+                        <img src="http://placehold.it/350x120">
+                    </a>
+                    <a href="#" class="thumbnail">
+                        <img src="http://placehold.it/350x120">
+                    </a>
+                    <a href="#" class="thumbnail">
+                        <img src="http://placehold.it/350x120">
+                    </a>                    
+                </div>
+                <div class="col-xs-6 col-md-3">
+                    <a href="#" class="thumbnail">
+                        <img src="http://placehold.it/350x120">
+                    </a>
+                    <a href="#" class="thumbnail">
+                        <img src="http://placehold.it/350x120">
+                    </a>
+                    <a href="#" class="thumbnail">
+                        <img src="http://placehold.it/350x120">
+                    </a>                    
+                </div>
+                <div class="col-xs-6 col-md-3">
+                    <a href="#" class="thumbnail">
+                        <img src="http://placehold.it/350x120">
+                    </a>
+                    <a href="#" class="thumbnail">
+                        <img src="http://placehold.it/350x120">
+                    </a>
+                    <a href="#" class="thumbnail">
+                        <img src="http://placehold.it/350x120">
+                    </a>                    
+                </div>
+            </div>
+            <nav> 
+                <ul  class= "pagination" > 
+                    <li> 
+                        <a  href= "#"  aria-label= "Anterior" > 
+                            <span  aria-hidden= "true" > « </span> 
+                        </a> 
+                    </li> 
+                    <li><a  href= "#" > 1 </a></li> 
+                    <li><a  href= "#" > 2 </a></li> 
+                    <li><a  href= "#" > 3 </a></li> 
+                    <li> 
+                        <a  href= "#"  aria-label= "Proxima" > 
+                            <span  aria-hidden= "true" > » </span> 
+                        </a> 
+                    </li> 
+                </ul> 
+            </nav>
+        </section>
--- index.php
+++ index.php
@@ -100,73 +100,8 @@
         </section>
 
         <?php include  './noticias/noticiasForm.php'; ?>
-
-        <section id="Fotos" class="container-fluid">
-            <h1>Fotos</h1>
-            <div class="row">
-                <div class="col-xs-6 col-md-3">
-                    <a href="#" class="thumbnail">
-                        <img src="http://placehold.it/350x120">
-                    </a>
-                    <a href="#" class="thumbnail">
-                        <img src="http://placehold.it/350x120">
-                    </a>
-                    <a href="#" class="thumbnail">
-                        <img src="http://placehold.it/350x120">
-                    </a>                    
-                </div>
-                <div class="col-xs-6 col-md-3">
-                    <a href="#" class="thumbnail">
-                        <img src="http://placehold.it/350x120">
-                    </a>
-                    <a href="#" class="thumbnail">
-                        <img src="http://placehold.it/350x120">
-                    </a>
-                    <a href="#" class="thumbnail">
-                        <img src="http://placehold.it/350x120">
-                    </a>                    
-                </div>
-                <div class="col-xs-6 col-md-3">
-                    <a href="#" class="thumbnail">
-                        <img src="http://placehold.it/350x120">
-                    </a>
-                    <a href="#" class="thumbnail">
-                        <img src="http://placehold.it/350x120">
-                    </a>
-                    <a href="#" class="thumbnail">
-                        <img src="http://placehold.it/350x120">
-                    </a>                    
-                </div>
-                <div class="col-xs-6 col-md-3">
-                    <a href="#" class="thumbnail">
-                        <img src="http://placehold.it/350x120">
-                    </a>
-                    <a href="#" class="thumbnail">
-                        <img src="http://placehold.it/350x120">
-                    </a>
-                    <a href="#" class="thumbnail">
-                        <img src="http://placehold.it/350x120">
-                    </a>                    
-                </div>
-            </div>
-            <nav> 
-                <ul  class= "pagination" > 
-                    <li> 
-                        <a  href= "#"  aria-label= "Anterior" > 
-                            <span  aria-hidden= "true" > « </span> 
-                        </a> 
-                    </li> 
-                    <li><a  href= "#" > 1 </a></li> 
-                    <li><a  href= "#" > 2 </a></li> 
-                    <li><a  href= "#" > 3 </a></li> 
-                    <li> 
-                        <a  href= "#"  aria-label= "Proxima" > 
-                            <span  aria-hidden= "true" > » </span> 
-                        </a> 
-                    </li> 
-                </ul> 
-            </nav>
-        </section>
+        <?php include  './fotosForm.php'; ?>
+        
         <section id="Videos" class="container-fluid text-center">
             <h1>Videos</h1>
             <iframe width="90%" height="80%" src="https://www.youtube.com/embed/videoseries?list=PLVpmN2T0ZVYXObtTizne3LU1hKKBgE5_U" frameborder="0" allowfullscreen></iframe>
